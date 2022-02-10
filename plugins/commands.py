65fff1b88852816b5ba875394a7bc216dd66e8a2
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-02-10 21:34:25 +0530

    Create commands.py

---
+++ plugins/commands.py
@@ -0,0 +1,75 @@
+import os
+from pyrogram import Client,filters 
+from telegraph import upload_file
+
+
+
+@Client.on_message(filters.command(["start"]))
+async def start(client, message):
+    await client.send_message(
+        chat_id=message.chat.id,
+        text=f"Hello {message.from_user.first_name},\n<b>I'm a Telegram To Telegra.ph Image/Video Uploader Bot. Created By @REX_BOTZ</b> \n<b>Do /help For More</b>",
+        reply_to_message_id=message.message_id
+    )
+
+@Client.on_message(filters.command(["help"]))
+async def help(client, message):
+    await client.send_message(
+        chat_id=message.chat.id,
+        text=f"<b>Send Me Any Video Or Photo I'll Upload It Into Telegra.ph.</b> \n<b>Created By @REX_BOTZ</b>",
+        reply_to_message_id=message.message_id
+    )
+    
+@Client.on_message(filters.photo)
+async def getimage(client, message):
+    location = "./FILES"
+    if not os.path.isdir(location):
+        os.makedirs(location)
+    imgdir = location + "/" + str(message.chat.id) + "/" + str(message.message_id) +".jpg"
+    dwn = await client.send_message(
+          text="<b>Downloading...</b>",
+          chat_id = message.chat.id,
+          reply_to_message_id=message.message_id
+          )          
+    await client.download_media(
+            message=message,
+            file_name=imgdir
+        )
+    await dwn.edit_text("<b>Uploading...</b>")
+    try:
+        response = upload_file(imgdir)
+    except Exception as error:
+        await dwn.edit_text(f"Oops Something Went Wrong\n{error} Contact @Rex_Bots_Support")
+        return
+    await dwn.edit_text(f"https://telegra.ph{response[0]}")
+    try:
+        os.remove(imgdir)
+    except:
+        pass
+
+@Client.on_message(filters.video)
+async def getvideo(client, message):
+    location = "./FILES"
+    if not os.path.isdir(location):
+        os.makedirs(location)
+    viddir = location + "/" + str(message.chat.id) + "/" + str(message.message_id) +".mp4"
+    dwn = await client.send_message(
+          text="<b>Downloading...</b>",
+          chat_id = message.chat.id,
+          reply_to_message_id=message.message_id
+          )          
+    await client.download_media(
+            message=message,
+            file_name=viddir
+        )
+    await dwn.edit_text("<b>Uploading...</b>")
+    try:
+        response = upload_file(viddir)
+    except Exception as error:
+        await dwn.edit_text(f"Oops Something Went Wrong\n{error} Contact @Rex_Bots_Support")
+        return
+    await dwn.edit_text(f"https://telegra.ph{response[0]}")
+    try:
+        os.remove(viddir)
+    except:
+        pass
