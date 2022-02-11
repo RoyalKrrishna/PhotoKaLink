93f1b26a9a2b533553f1414deb0116cb8446de60
Royal Krrishna <91977328+RoyalKrrishna@users.noreply.github.com>
2022-02-11 23:03:20 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -68,7 +68,11 @@ async def getvideo(client, message):
     except Exception as error:
         await dwn.edit_text(f"Oops Something Went Wrong\n{error} Contact <b>@Royalkrrishna 🕵️</b>")
         return
-    await dwn.edit_text(f"https://telegra.ph{response[0]}")
+    await dwn.edit_text
+      (
+      f"https://telegra.ph{response[0]}"
+      disable_web_page_preview=True
+      )
     try:
         os.remove(viddir)
     except:
